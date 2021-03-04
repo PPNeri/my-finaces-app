e33c3facd2ba576378723d90742e7ad0f00a5ab6
PPNeri <pedroneriti@gmail.com>
2021-03-03 23:21:53 -0300

    Finalizandurl heroku

--- src/views/home.js
+++ src/views/home.js
@@ -39,7 +39,7 @@ class Home extends Component {
                 <hr className="my-4" />
                 <p>E essa é sua área administrativa, utilize um dos menus ou botões abaixo para navegar pelo sistema.</p>
                 <p className="lead">
-                    <a className="btn btn-primary btn-lg" href="https://bootswatch.com/flatly/#" role="button">
+                    <a className="btn btn-primary btn-lg" href="#/cadastro-usuarios" role="button">
                         <i className="pi pi-users"></i>  Cadastrar Usuário
                     </a>
                     <a
